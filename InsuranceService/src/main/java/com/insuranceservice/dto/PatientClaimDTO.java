ba81e77283c46d3ddd1fc97e6a8589e740c224fa
HP <HP@LAPTOP-0G1LEEOR>
2023-07-14 12:37:02 +0530

    Insurance Service : AOP and Exception Handling

--- InsuranceService/src/main/java/com/insuranceservice/dto/PatientClaimDTO.java
+++ InsuranceService/src/main/java/com/insuranceservice/dto/PatientClaimDTO.java
@@ -5,7 +5,6 @@ import java.util.UUID;
 import com.insuranceservice.entity.Insurance;
 import com.insuranceservice.util.Constants;
 
-import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -16,18 +15,11 @@ import lombok.NoArgsConstructor;
 @Data
 public class PatientClaimDTO implements Constants {
 	
-	@NotNull(message = NOT_NULL)
-	@NotEmpty(message = NOT_EMPTY)
 	private UUID patientId;
 	
 	@NotNull(message = NOT_NULL)
 	private Insurance insuranceTaken;
 	
-	@NotNull(message = NOT_NULL)
-	@NotEmpty(message = NOT_EMPTY)
 	private Double amountSpent;
-	
-	@NotNull(message = NOT_NULL)
-	@NotEmpty(message = NOT_EMPTY)
 	private Double remainingAmount;
 }
---
+++ InsuranceService/src/main/java/com/insuranceservice/exception/InsurerNotFoundException.java
@@ -0,0 +1,11 @@
+package com.insuranceservice.exception;
+
+public class InsurerNotFoundException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public InsurerNotFoundException(String msg) {
+		super(msg);
+	}
+
+}
