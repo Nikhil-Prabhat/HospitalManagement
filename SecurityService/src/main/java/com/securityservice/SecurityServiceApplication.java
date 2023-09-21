6f4216cca45902e51ffeca948d01f420dc0a4883
Nikhil_Prabhat <nikhilprabhat03051999@gmail.com>
2023-09-21 17:19:30 +0530

    Security Service : Cors changes

--- SecurityService/src/main/java/com/securityservice/SecurityServiceApplication.java
+++ SecurityService/src/main/java/com/securityservice/SecurityServiceApplication.java
@@ -2,17 +2,16 @@ package com.securityservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 
 @SpringBootApplication
-@OpenAPIDefinition(info = @Info(
-		contact = @Contact(name = "Nikhil Prabhat", email = "strangernikhilprabhat98@gmail.com"), 
-		title = "Security Service", 
-		version = "1.0", 
-		description = "OpenAPI Documentation For Security Service"))
+@OpenAPIDefinition(info = @Info(contact = @Contact(name = "Nikhil Prabhat", email = "strangernikhilprabhat98@gmail.com"), title = "Security Service", version = "1.0", description = "OpenAPI Documentation For Security Service"))
 public class SecurityServiceApplication {
 
 	public static void main(String[] args) {
