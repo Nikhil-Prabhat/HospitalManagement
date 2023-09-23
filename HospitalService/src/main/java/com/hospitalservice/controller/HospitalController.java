c0f059c42b6be9bedca6d36aca9bc613a038bedc
Nikhil_Prabhat <nikhilprabhat03051999@gmail.com>
2023-09-23 20:48:45 +0530

    Hospital Service : CORS Issue Fixed

--- HospitalService/src/main/java/com/hospitalservice/controller/HospitalController.java
+++ HospitalService/src/main/java/com/hospitalservice/controller/HospitalController.java
@@ -6,6 +6,7 @@ import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -39,6 +40,7 @@ import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping(Constants.HOSPITAL_APP)
+@CrossOrigin(origins = "*")
 public class HospitalController implements Constants {
 
 	@Autowired
